0927ae2e4f58ba90699c37ac172bdda9365c1fe6
Ralph818 <ralph818@hotmail.com>
2021-07-26 15:24:11 -0500

    Se agrega el quiz de dibujar canvas

--- Interactions/InteractionsDemo.cs
+++ Interactions/InteractionsDemo.cs
@@ -6,6 +6,7 @@ using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -28,6 +29,7 @@ namespace UnitTestProject1.Interactions
         {
             new DriverManager().SetUpDriver(new ChromeConfig());
             driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
             actions = new Actions(driver);
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
         }
@@ -38,10 +40,11 @@ namespace UnitTestProject1.Interactions
             driver.Close();
             driver?.Quit();
         }
-        [Category ("DragAndDrop")]
+
+        [Category("DragAndDrop")]
         [Test]
         public void DragDropExample1()
-        {            
+        {
             driver.Navigate().GoToUrl("https://jqueryui.com/droppable/");
             wait.Until(FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
 
@@ -49,7 +52,7 @@ namespace UnitTestProject1.Interactions
             IWebElement targetElement = driver.FindElement(By.Id("droppable"));
 
             actions.DragAndDrop(sourceElement, targetElement).Perform();
-            
+
             Thread.Sleep(3000);
             Assert.AreEqual("Dropped!", targetElement.Text);
         }
@@ -75,7 +78,7 @@ namespace UnitTestProject1.Interactions
                 .Release(sourceElement)
                 .Build();
             drag.Perform();*/
-            
+
 
             Thread.Sleep(3000);
             Assert.AreEqual("Dropped!", targetElement.Text);
@@ -98,5 +101,73 @@ namespace UnitTestProject1.Interactions
             Thread.Sleep(3000);
         }
 
+        [Test]
+        public void Resizable()
+        {
+            driver.Navigate().GoToUrl("https://jqueryui.com/resizable/");
+            wait.Until(FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
+            IWebElement webElement = driver.FindElement(By.XPath("//*[@class = 'ui-resizable-handle ui-resizable-se ui-icon ui-icon-gripsmall-diagonal-se']"));
+
+            actions.ClickAndHold(webElement).DragAndDropToOffset(webElement, 50, 50).Perform();
+            Assert.IsTrue(driver.FindElement(By.XPath("//*[@id='resizable' and @style]")).Displayed);
+
+        }
+
+        [Test]
+        public void OpenNetworksTab()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            actions.KeyDown(Keys.Control).KeyDown(Keys.Shift).SendKeys("q").Perform();
+
+            //actions.SendKeys(Keys.F12).Perform();
+
+            actions.KeyUp(Keys.Control).KeyUp(Keys.Shift).Perform();
+            driver.Navigate().GoToUrl("https://www.ultimateqa.com");
+
+            Thread.Sleep(3000);
+        }  //This doesn't work in Chrome
+
+        [Test]
+        public void DragAndDropHTML5()
+        {
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/drag_and_drop");
+            IWebElement elementA = driver.FindElement(By.Id("column-a"));
+            IWebElement elementB = driver.FindElement(By.Id("column-b"));
+
+            //Import javascript file and execute it to simulate drag and drop for HTML5
+            var jsFile = File.ReadAllText(@"C:\Users\YamiletGIGA\source\repos\UnitTestProject1\drag_and_drop_helper.js");
+            IJavaScriptExecutor javaScript = driver as IJavaScriptExecutor;
+            //Execute javascript - #{{id value}}
+            javaScript.ExecuteScript(jsFile + "$('#column-a').simulateDragDrop({ dropTarget: '#column-b'});");
+
+            Assert.AreEqual("B", elementA.Text);
+
+            Thread.Sleep(2000);
+        }
+
+        [Test]
+        public void DrawInCanvas()
+        {
+            driver.Navigate().GoToUrl("https://compendiumdev.co.uk/selenium/gui_user_interactions.html");
+            wait.Until(ElementIsVisible(By.Id("canvas")));
+           
+            IWebElement canvas = driver.FindElement(By.Id("canvas"));
+            actions.MoveToElement(canvas).ClickAndHold().Perform();
+            //to draw a V
+            int y;
+            for (int x=1; x<=20; x++)
+            {
+                if (x<=10)
+                    y = 1;
+                else
+                    y = -1;
+ 
+                actions.MoveByOffset(1,y).Perform();
+                actions.Release().Perform();
+                actions.ClickAndHold().Perform();
+            }
+            actions.Release();
+            Assert.IsTrue(driver.FindElement(By.XPath("//*[@id='keyeventslist']/li")).Displayed);
+        }
     }
 }
